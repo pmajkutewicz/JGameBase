9c6ec4246a6049808d239a59d9dae1b8b8a29cf8
Paweł Majkutewicz <pmajkutewicz@gmail.com>
2017-03-09 23:43:08 +0100

    Manual array copy replaced by System.arraycopy(...)

--- src/jgamebase/plugins/extractor/sevenzip/SevenZip/Compression/LZ/InWindow.java
+++ src/jgamebase/plugins/extractor/sevenzip/SevenZip/Compression/LZ/InWindow.java
@@ -33,9 +33,7 @@ public class InWindow {
     final int numBytes = (_bufferOffset + _streamPos) - offset;
 
     // check negative offset ????
-    for (int i = 0; i < numBytes; i++) {
-      _bufferBase[i] = _bufferBase[offset + i];
-    }
+    System.arraycopy(_bufferBase, offset + 0, _bufferBase, 0, numBytes);
     _bufferOffset -= offset;
   }
 
